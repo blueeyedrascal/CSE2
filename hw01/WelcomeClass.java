6d1d562e1ae9277aa7609582f47cd60775341c87
blueeyedrascal <amm315@lehigh.edu>
2015-01-23 15:28:25 +0000

    updated HelloWorld

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,26 @@
+////////////////////////////////////////////////////////////////////
+// Allison Moroz 
+// Homework 1
+// Welcome Class Java Program
+//
+//  define class
+public class WelcomeClass{
+    
+//  main method
+    public static void main(String[] args) {
+    
+//  print out the pattern
+    System.out.println("-----------");
+    System.out.println("| WELCOME |");
+    System.out.println("-----------");
+    System.out.println("  ^  ^  ^  ^  ^  ^");
+    System.out.println(" / \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("<-A--M--M--3--1--5->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println("This fall I choreographed the student produced show Reefer Madness");
+    System.out.println("It was an interesting experience.");
+    
+    }
+    
+}
